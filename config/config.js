14fde8304383aa8841aac8d7e2be9adea2348753
djtucker123 <212443631@logon.ds.ge.com>
2019-01-24 18:05:52 -0500

    sqlize configuration updates

--- config/config.js
+++ config/config.js
@@ -1,15 +1,15 @@
-
 module.exports = {
-  // development: {
-  //   password: process.env.SQL_KEY,
-  //   username: "root",
-  //   database: "bamazon",
-  //   host: "localhost",
-  //   port: 3306,
-  //   dialect: "mysql"
-  // },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    dialect: "mysql"
-  }
-};
+    "development": {
+        "username": "root",
+        "password": process.env.SQL_KEY,
+        "database": "bamazon",
+        "host": "localhost",
+        "port": 3306,
+        "dialect": "mysql"
+    },
+    
+    "production": {
+        "use_env_variable": "JAWSDB_URL",
+        "dialect": "mysql"
+    }
+}
--- models/index.js
+++ models/index.js
@@ -5,10 +5,10 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "production";
+var env = process.env.NODE_ENV || "development";
 console.log(env);
 
-var config = require(__dirname + "/../config/config.js")[env];
+var config = require(__dirname + "/../config/config")[env];
 var db = {};
 
 
@@ -16,7 +16,7 @@ var db = {};
 //   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 // } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+//}
 
 fs
   .readdirSync(__dirname)
